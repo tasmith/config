b7530a017f64f4b8822fecaea7f5680fc97580ea
Todd Smith <t@ts6.us>
2013-04-23 22:06:31 -0500

    First pass at org-mode based configuration. init.org

---
+++ emacs.d/emacs-custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inferior-js-program-command "/usr/local/bin/rhino")
+ '(safe-local-variable-values (quote ((virtualenv-default-directory . "/Users/todd/Projects") (virtualenv-workon . "py331")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
